e8286be19ed04325322d4cd980b222dd5b8e2609
Andrew Mustea <amustea224@gmail.com>
2022-08-14 23:13:09 -0700

    nvim: add shellcheck arg '-x' to ale config

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -1,17 +1,23 @@
 -- ale
 --
+local g = vim.g
 
+-- ale settings
+g.ale_disable_lsp = 1
+g.ale_lint_on_text_changes = 0
+g.ale_lint_on_insert_leave = 0
+g.ale_lint_on_save = 1
+g.ale_lint_on_filetype_changed = 1
+g.ale_lint_on_enter = 1
+
+-- include luacheckrc if available
 if vim.bo.filetype:match("lua") then
   local luacheckrc = vim.fn.expand("%:p:h") .. "/.luacheckrc"
   if vim.fn.filereadable(luacheckrc) then
-    vim.g.ale_lua_luacheck_options = "--config " .. luacheckrc
+    g.ale_lua_luacheck_options = "--config " .. luacheckrc
   end
 end
 
-vim.g.ale_disable_lsp = 1
-vim.g.ale_lint_on_text_changes = 0
-vim.g.ale_lint_on_insert_leave = 0
-vim.g.ale_lint_on_save = 1
-vim.g.ale_lint_on_filetype_changed = 1
-vim.g.ale_lint_on_enter = 1
+-- disable certain ShellCheck rules
+g.ale_sh_shellcheck_options = "-x"
 
