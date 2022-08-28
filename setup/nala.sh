1d54dae73fee1c1604faad61fcceee539d799056
Andrew Mustea <amustea224@gmail.com>
2022-08-28 15:17:33 -0700

    setup: nala: update nala apt commands
    - nala.sh uses the recommended install commands from the nala git
      repository.
    - Also updated the distro check function to work on all distros.

--- setup/nala.sh
+++ setup/nala.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if [ "$(lsb_release -i | awk '{ print $3 }')" != "Ubuntu" ]; then
+DISTRO="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
+if [ "$DISTRO" != "ubuntu" ]; then
     echo "Nala requires an Ubuntu release"
     exit
 fi
@@ -11,17 +12,13 @@ if [ "$RELEASE" -lt 20 ]; then
     exit
 fi
 
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run as root"
-    exit
-fi
-
-echo "deb https://deb.volian.org/volian/ scar main" > /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-wget -qO - https://deb.volian.org/volian/scar.key > /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
+echo "deb https://deb.volian.org/volian/ scar main" | \
+    command sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | \
+    command sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 
 apt update
 
-RELEASE=$(lsb_release -r | awk '{ split($2, a, "."); print a[1] }')
 if [ "$RELEASE" -lt 22 ]; then
     echo "Installing nala-legacy..."
     apt install nala-legacy
