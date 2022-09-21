63432115d5450744c408695c6e19cdf5cff54d4d
Andrew Mustea <amustea224@gmail.com>
2022-09-21 15:16:35 -0700

    nvim: packer: fix spacing format

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,9 +5,9 @@
 -- automatically run :PackerCompile whenever plugins.lua is updated
 --
 vim.api.nvim_create_autocmd('BufWritePost', {
-    group = vim.api.nvim_create_augroup('PACKER', { clear = true }),
-    pattern = 'plugins.lua',
-    command = 'source <afile> | PackerCompile',
+  group = vim.api.nvim_create_augroup('PACKER', { clear = true }),
+  pattern = 'plugins.lua',
+  command = 'source <afile> | PackerCompile',
 })
 
 
