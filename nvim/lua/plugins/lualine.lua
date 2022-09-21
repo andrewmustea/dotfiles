426eca391d69126ed408b77010aeb8715479c172
Andrew Mustea <amustea224@gmail.com>
2022-09-21 15:12:18 -0700

    nvim: lualine: add colored file status marker
    - Blue for modified and red for read-only/unmodifiable.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,25 @@
 -- lualine.nvim
 --
 
+local bo = vim.bo
+local status_color = nil
+
+local function file_status()
+  if bo.modifiable == false or bo.readonly == true then
+    status_color = { fg = "#b02828" }
+    return "[-]"
+  elseif bo.modified then
+    status_color = { fg = "#0040bb" }
+    return "[+]"
+  end
+  status_color = nil
+  return ""
+end
+
+local function get_color()
+  return status_color
+end
+
 require("lualine").setup {
   options = {
     icons_enabled = true,
@@ -12,21 +31,44 @@ require("lualine").setup {
     globalstatus = false,
   },
   sections = {
-    lualine_a = {"mode"},
-    lualine_b = {"branch", "diff", "diagnostics"},
-    lualine_c = {"filename"},
-    lualine_d = {require("auto-session-library").current_session_name},
-    lualine_x = {"filetype"},
-    lualine_y = {"progress"},
-    lualine_z = {"location"}
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = {
+      { "filename",
+        file_status = false,
+        newfile_status = false,
+        path = 1,
+        separator = ""
+      },
+      { file_status,
+        color = get_color,
+        separator = { left = "", right = "|" },
+        padding = { left = 0, right = 1 }
+      },
+    },
+    lualine_x = { "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" }
   },
   inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {"filename"},
-    lualine_x = {"location"},
-    lualine_y = {},
-    lualine_z = {}
+    lualine_a = { },
+    lualine_b = { },
+    lualine_c = {
+      { "filename",
+        file_status = false,
+        newfile_status = false,
+        path = 1,
+        separator = ""
+      },
+      { file_status,
+        color = get_color,
+        separator = { left = "", right = "|" },
+        padding = { left = 0, right = 1 }
+      },
+    },
+    lualine_x = { "location" },
+    lualine_y = { },
+    lualine_z = { }
   },
   extensions = { "nvim-tree" }
 }
