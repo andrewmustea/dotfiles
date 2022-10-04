6e77bc0f5a74e2eba8d8444692e05b388047d1c7
Andrew Mustea <amustea224@gmail.com>
2022-10-04 10:39:18 -0700

    fzf: update colorscheme

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -20,7 +20,7 @@ source "$XDG_DATA_HOME/fzf/shell/key-bindings.bash"
 # Colors
 # ------
 FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
-    --color=fg:#888888,bg:#0c0c0c,hl:#b030a0 \
+    --color=fg:#888888,hl:#b030a0 \
     --color=fg+:#bbbbbb,bg+:#151a1e,hl+:#36a3d9 \
     --color=info:#508040,prompt:#8030e0,pointer:#f06722 \
     --color=marker:#0040bb,spinner:#b02828,header:#0078c8"
