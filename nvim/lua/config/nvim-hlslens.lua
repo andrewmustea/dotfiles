049d44f8f3797322c8b459b323e1e30b6b5c92fa
Andrew Mustea <amustea224@gmail.com>
2022-10-24 00:57:17 -0700

    nvim: nvim-hlslens: fix syntax

--- nvim/lua/config/nvim-hlslens.lua
+++ nvim/lua/config/nvim-hlslens.lua
@@ -1,7 +1,7 @@
 -- nvim-hlslens
 --
 
-local api = vim.api
+local map = vim.keymap.set
 local kopts = { noremap = true, silent = true }
 
 require("hlslens").setup({
@@ -10,16 +10,11 @@ require("hlslens").setup({
   end,
 })
 
-api.nvim_set_keymap("n", "n",
-  [[<Cmd>execute("normal! " . v:count1 . "n")<CR><Cmd>lua require("hlslens").start()<CR>]],
-  kopts)
-api.nvim_set_keymap("n", "N",
-  [[<Cmd>execute("normal! " . v:count1 . "N")<CR><Cmd>lua require("hlslens").start()<CR>]],
-  kopts)
-api.nvim_set_keymap("n", "*", [[*<Cmd>lua require("hlslens").start()<CR>]], kopts)
-api.nvim_set_keymap("n", "#", [[#<Cmd>lua require("hlslens").start()<CR>]], kopts)
-api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require("hlslens").start()<CR>]], kopts)
-api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require("hlslens").start()<CR>]], kopts)
-
-api.nvim_set_keymap("n", "<Leader>l", ":noh<CR>", kopts)
+map("n", "n", "<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>", kopts)
+map("n", "N", "<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>", kopts)
+map("n", "*", "*<Cmd>lua require('hlslens').start()<CR>", kopts)
+map("n", "#", "#<Cmd>lua require('hlslens').start()<CR>", kopts)
+map("n", "g*", "g*<Cmd>lua require('hlslens').start()<CR>", kopts)
+map("n", "g#", "g#<Cmd>lua require('hlslens').start()<CR>", kopts)
+map("n", "<Leader>l", ":noh<CR>", kopts)
 
