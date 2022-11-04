aefa0ff5847bc09c1288f4552281fb997263b2e1
Andrew Mustea <amustea224@gmail.com>
2022-11-03 22:01:45 -0700

    etc: profile.d: arch.sh: move fzf and nvm from bashrc

--- etc/profile.d/arch.sh
+++ etc/profile.d/arch.sh
@@ -17,3 +17,10 @@ alias pac='"$PACKAGE_MANAGER"'
 alias pacsearch='"$PACKAGE_MANAGER" -Ss'
 alias pacquery='"$PACKAGE_MANAGER" -Q'
 
+# fzf
+[ -f "$CONFIG/fzf/fzf.bash" ] && . "$CONFIG/fzf/fzf.bash"
+
+# nvm
+export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+[ -d "$NVM_DIR" ] && . "$NVM_DIR/nvm.sh"
+
