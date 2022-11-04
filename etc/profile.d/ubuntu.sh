7b3a56464ea22448b36296db03b96dd96f2b4c63
Andrew Mustea <amustea224@gmail.com>
2022-11-03 17:28:35 -0700

    etc: profile.d: ubuntu.sh: move fzf and nvm to ubuntu.sh

--- etc/profile.d/ubuntu.sh
+++ etc/profile.d/ubuntu.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # /etc/profile.d/ubuntu.sh
 #
 
 # golang
-GO_BIN=/usr/local/go/bin
-if [ -d "$GO_BIN" ]; then
+export GO_BIN=/usr/local/go/bin
+if [[ -d "$GO_BIN" ]]; then
     case ":$PATH:" in
         *:"$GO_BIN":*)
             ;;
@@ -15,11 +15,32 @@ if [ -d "$GO_BIN" ]; then
     esac
 fi
 
+# fzf
+export FZF_DIR="$DATA/fzf"
+export FZF_ENV="$CONFIG/fzf/fzf.bash"
+[[ -f "$FZF_ENV" ]] && [[ -x "$FZF_DIR/bin/fzf" ]] && source "$FZF_ENV"
+
+update-fzf() {
+    if ! [[ -d $FZF_DIR ]]; then
+        echo "Couldn't find fzf direcotry in '$FZF_DIR'"
+        return 1
+    fi
+    git -C "$FZF_DIR" pull
+    "$FZF_DIR/install --xdg --bin"
+}
+
 # nala
-which nala >/dev/null 2>&1 && alias apt='nala'
+which nala &>/dev/null && alias apt='nala'
+
+# nvm
+export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+if [[ -d "$NVM_DIR" ]]; then
+    source "$NVM_DIR/nvm.sh"
+    source "$NVM_DIR/bash_completion"
+fi
 
 # Set PATH, MANPATH, etc., for Homebrew
-if [ -x /home/linuxbrew/.linuxbrew/bin/brew ]; then
+if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
