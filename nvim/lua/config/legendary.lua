ec4bc425e3674f3820eb18dfd2602941b9dcf02e
Andrew Mustea <amustea224@gmail.com>
2022-11-14 11:00:25 -0800

    nvim: legendary.nvim: update config

--- nvim/lua/config/legendary.lua
+++ nvim/lua/config/legendary.lua
@@ -2,71 +2,62 @@
 --
 
 require("legendary").setup {
+  -- Initial keymaps to bind
+  keymaps = {},
+  -- Initial commands to bind
+  commands = {},
+  -- Initial augroups/autocmds to bind
+  autocmds = {},
+  -- Initial functions to bidn
+  functions = {},
+  -- Customize the prompt that appears on your vim.ui.select() handler
+  -- Can be a string or a function that returns a string.
+  select_prompt = ' legendary.nvim ',
+  -- Character to use to separate columns in the UI
+  col_separator_char = '│',
+  -- Optionally pass a custom formatter function. This function
+  -- receives the item as a parameter and the mode that legendary
+  -- was triggered from (e.g. `function(item, mode): string[]`)
+  -- and must return a table of non-nil string values for display.
+  -- It must return the same number of values for each item to work correctly.
+  -- The values will be used as column values when formatted.
+  -- See function `default_format(item)` in
+  -- `lua/legendary/ui/format.lua` to see default implementation.
+  default_item_formatter = nil,
   -- Include builtins by default, set to false to disable
   include_builtin = true,
   -- Include the commands that legendary.nvim creates itself
   -- in the legend by default, set to false to disable
   include_legendary_cmds = true,
-  -- Customize the prompt that appears on your vim.ui.select() handler
-  -- Can be a string or a function that takes the `kind` and returns
-  -- a string. See "Item Kinds" below for details. By default,
-  -- prompt is "Legendary" when searching all items,
-  -- "Legendary Keymaps" when searching keymaps,
-  -- "Legendary Commands" when searching commands,
-  -- and "Legendary Autocmds" when searching autocmds.
-  select_prompt = nil,
-  -- Optionally pass a custom formatter function. This function
-  -- receives the item as a parameter and the mode that legendary
-  -- was triggered from (e.g. `function(item, mode): {string}`)
-  -- and must return a table of non-nil string values for display.
-  -- It must return the same number of values for each item to work correctly.
-  -- The values will be used as column values when formatted.
-  -- See function `get_default_format_values(item)` in
-  -- `lua/legendary/formatter.lua` to see default implementation.
-  formatter = nil,
-  col_separator_char = "│",
-  -- When you trigger an item via legendary.nvim,
-  -- show it at the top next time you use legendary.nvim
-  most_recent_item_at_top = true,
-  -- Initial keymaps to bind
-  keymaps = {
-    -- your keymap tables here
-  },
-  -- Initial commands to bind
-  commands = {
-    -- your command tables here
-  },
-  -- Initial functions to bind
-  functions = {
-    -- your function tables here
-  },
-  -- Initial augroups and autocmds to bind
-  autocmds = {
-    -- your autocmd tables here
-  },
+  -- Sort most recently used items to the top of the list
+  -- so they can be quickly re-triggered when opening legendary again
+  most_recent_items_at_top = true,
   which_key = {
+    -- Automatically add which-key tables to legendary
+    -- see ./doc/WHICH_KEY.md for more details
+    auto_register = false,
     -- you can put which-key.nvim tables here,
     -- or alternatively have them auto-register,
-    -- see section on which-key integration
+    -- see ./doc/WHICH_KEY.md
     mappings = {},
     opts = {},
     -- controls whether legendary.nvim actually binds they keymaps,
     -- or if you want to let which-key.nvim handle the bindings.
     -- if not passed, true by default
-    do_binding = {},
+    do_binding = true
   },
-  -- Automatically add which-key tables to legendary
-  -- see "which-key.nvim Integration" below for more details
-  auto_register_which_key = false,
-  -- settings for the :LegendaryScratch command
   scratchpad = {
-    -- configure how to show results of evaluated Lua code,
-    -- either "print" or "float"
-    -- Pressing q or <ESC> will close the float
-    display_results = "float",
-    -- cache the contents of the scratchpad to a file and restore it
-    -- next time you open the scratchpad
-    cache_file = string.format("%s/%s", vim.fn.stdpath("cache"), "legendary_scratch.lua"),
+    -- How to open the scratchpad buffer,
+    -- 'current' for current window, 'float'
+    -- for floating window
+    view = 'float',
+    -- How to show the results of evaluated Lua code.
+    -- 'print' for `print(result)`, 'float' for a floating window.
+    results_view = 'float',
+    -- Border style for floating windows related to the scratchpad
+    float_border = 'rounded',
+    -- Whether to restore scratchpad contents from a cache file
+    keep_contents = true
   }
 }
 
