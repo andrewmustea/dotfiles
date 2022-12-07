207125924e4cbbaaaa409b1ed490541eb9e138e1
Andrew Mustea <amustea224@gmail.com>
2022-12-07 14:15:23 -0800

    nvim: nvim-scrollbar: set colors for scrollbar
    - Update scrollbar autocmd for search handler.

--- nvim/lua/config/nvim-scrollbar.lua
+++ nvim/lua/config/nvim-scrollbar.lua
@@ -3,13 +3,38 @@
 
 local api = vim.api
 
-require("scrollbar").setup({
-  api.nvim_create_autocmd("CmdlineLeave", {
-    group = api.nvim_create_augroup("scrollbar_search_hide", { clear = true }),
-    callback =
-      function()
-        api.nvim_command("lua require('scrollbar.handlers.search').handler.hide()")
-      end
-  })
-})
+require("scrollbar").setup {
+  handle = {
+    text = " ",
+    color = "#202020",
+    cterm = nil,
+    highlight = "CursorColumn",
+    hide_if_all_visible = true,
+  },
+  marks = {
+    Cursor = {
+        text = "•",
+        priority = 0,
+        color = "#888888",
+        cterm = nil,
+        highlight = "Normal"
+    }
+  },
+  handlers = {
+    cursor = true,
+    diagnostic = true,
+    gitsigns = true,
+    handle = true,
+    search = false
+  }
+}
+
+api.nvim_create_autocmd("CmdlineLeave", {
+  group = api.nvim_create_augroup("scrollbar_search_hide", { clear = true }),
+  callback =
+    function()
+      return require('scrollbar.handlers.search').handler.hide()
+    end
+  }
+)
 
