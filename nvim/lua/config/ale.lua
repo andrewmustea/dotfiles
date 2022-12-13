19ff1b9ef24428f8d157eccf2a68af60c29316ce
Andrew Mustea <amustea224@gmail.com>
2022-12-12 16:56:57 -0800

    nvim: ale: disable ale for certain filetypes
    - Disable ale for lua, python, and sh

--- nvim/lua/config/ale.lua
+++ nvim/lua/config/ale.lua
@@ -10,14 +10,11 @@ g.ale_lint_on_save = 1
 g.ale_lint_on_filetype_changed = 1
 g.ale_lint_on_enter = 1
 
--- include luacheckrc if available
-if vim.bo.filetype:match("lua") then
-  local luacheckrc = vim.fn.expand("%:p:h") .. "/.luacheckrc"
-  if vim.fn.filereadable(luacheckrc) then
-    g.ale_lua_luacheck_options = "--config " .. luacheckrc
-  end
-end
-
--- disable certain ShellCheck rules
-g.ale_sh_shellcheck_options = "-x"
+-- linters
+-- g.ale_linters_explicit = 1
+g.ale_linters = {
+  ["lua"] = { },
+  ["python"] = { },
+  ["sh"] = { }
+}
 
