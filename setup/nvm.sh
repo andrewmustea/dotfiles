ee42ef30b476abd5df444bad9d27879714ab019e
Andrew Mustea <amustea224@gmail.com>
2022-12-28 15:03:22 -0800

    setup: nvm: clean up script

--- setup/nvm.sh
+++ setup/nvm.sh
@@ -1,27 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+
+if declare -f -F nvm > /dev/null; then
+    echo "nvm already installed"
+    exit 0
+fi
 
 DISTRO="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
 
-[ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME=~/.config
+[[ -z "${XDG_CONFIG_HOME}" ]] && export XDG_CONFIG_HOME="${HOME}/.config"
 
-export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+export NVM_DIR="${XDG_CONFIG_HOME}/nvm"
 
-if which nvm >/dev/null 2>&1; then
-    echo "nvm already installed"
-    if which node >/dev/null 2>&1; then
-        echo "node already installed"
-        exit
-    fi
-elif [ "$DISTRO" = "arch" ]; then
+if [[ "$DISTRO" = "arch" ]]; then
     sudo pacman -S --needed --noconfirm nvm
-    mkdir --parents "$XDG_CONFIG_HOME/nvm"
-    . /usr/share/nvm/init-nvm.sh
-else
-    git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+    mkdir --parents "${NVM_DIR}"
+    source /usr/share/nvm/init-nvm.sh
+    exit 0
 fi
+
+git clone https://github.com/nvm-sh/nvm.git "${NVM_DIR}"
+
+[[ -s "${NVM_DIR}/nvm.sh" ]] && source "${NVM_DIR}/nvm.sh"
+[[ -s "${NVM_DIR}/bash_completion" ]] && source "${NVM_DIR}/bash_completion"
 
 nvm install node --default --latest-npm
 
