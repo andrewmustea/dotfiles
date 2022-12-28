55fdd13825734329521b70929634e77ac8e4dfbb
Andrew Mustea <amustea224@gmail.com>
2022-12-28 15:02:27 -0800

    setup: lua-lsp: clean up script

--- setup/lua-lsp.sh
+++ setup/lua-lsp.sh
@@ -1,43 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-if which lua-language-server >/dev/null 2>&1; then
+if hash lua-language-server &>/dev/null; then
     echo "lua-language-server already installed"
     exit
 fi
 
-[ -z "$XDG_DATA_HOME" ] && export XDG_DATA_HOME=~/.local/share
-
 DISTRO="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
 
-if [ "$DISTRO" = "arch" ]; then
+if [[ "${DISTRO}" == "arch" ]]; then
     sudo pacman -S --needed --noconfirm lua-language-server
-    exit
-elif [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "ubuntu" ]; then
-    echo "Distro not supported: $DISTRO"
+    exit 0
+fi
+
+if [[ "${DISTRO}" != "debian" ]] && [[ "${DISTRO}" != "ubuntu" ]]; then
+    echo "distro not supported: ${DISTRO}"
     exit 1
 fi
 
-if which nala >/dev/null 2>&1; then
+[[ -z "${XDG_DATA_HOME}" ]] && export XDG_DATA_HOME="${HOME}/.local/share"
+
+if hash nala &>/devl/null; then
     sudo nala install -y ninja-build
 else
     sudo apt install -y ninja-build
 fi
 
-git clone --depth=1 https://github.com/sumneko/lua-language-server \
-    "$XDG_DATA_HOME/lua-language-server"
-cd "$XDG_DATA_HOME/lua-language-server" || exit 1
-git submodule update --depth 1 --init --recursive
+lsp_dir="${XDG_DATA_HOME}/lua-language-server"
 
-cd 3rd/luamake || exit 1
+git clone --depth=1 https://github.com/sumneko/lua-language-server "${lsp_dir}"
+git -C "${lsp_dir}" submodule update --depth 1 --init --recursive
+
+cd "${lsp_dir}3rd/luamake" || exit 1
 ./compile/install.sh
 cd ../..
 ./3rd/luamake/luamake rebuild
 
-case ":$PATH:" in
-    *:"$XDG_DATA_HOME/lua-language-server/bin":*)
-        ;;
-    *)
-        export PATH="${PATH:+${PATH}:}$XDG_DATA_HOME/lua-language-server/bin"
-        ;;
-esac
+if [[ ":${PATH}:" != ":${lsp_dir}/bin:" ]]; then
+    export PATH="${PATH:+${PATH}:}${lsp_dir}/bin"
+fi
 
