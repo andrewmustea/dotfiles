66c1b7aba59889560dfd2c4623ea3563015e199d
Andrew Mustea <amustea224@gmail.com>
2022-12-28 15:02:56 -0800

    setup: nala: clean up script

--- setup/nala.sh
+++ setup/nala.sh
@@ -1,35 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-if which nala >/dev/null 2>&1; then
+if hash nala &>/dev/null; then
     echo "nala already installed"
-    exit
+    exit 0
 fi
 
 DISTRO="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
 
-if [ "$DISTRO" = "debian" ]; then
-    sudo apt install -y nala
-    exit
-elif [ "$DISTRO" != "ubuntu" ]; then
+unset legacy
+if [[ "${DISTRO}" != "ubuntu" ]]; then
+    RELEASE=$(lsb_release -r | awk '{ split($2, a, "."); print a[1] }')
+    if (( RELEASE < 20 )); then
+        echo "nala requires Ubuntu release >=20.04"
+        exit 1
+    elif (( RELEASE < 22 )); then
+        legacy=true
+    fi
+elif [[ "${DISTRO}" != "debian" ]]; then
     echo "nala requires a debian-based distro"
-    exit 1
-fi
-
-RELEASE=$(lsb_release -r | awk '{ split($2, a, "."); print a[1] }')
-if [ "$RELEASE" -lt 20 ]; then
-    echo "nala requires Ubuntu release >=20.04"
     exit 1
 fi
 
 echo "deb https://deb.volian.org/volian/ scar main" | \
     sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | \
-    sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > \
-    /dev/null
+    sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 
 sudo apt update
 
-if [ "$RELEASE" -lt 22 ]; then
+if [[ -z "${legacy}" ]]; then
     echo "Installing nala-legacy..."
     sudo apt install -y nala-legacy
 else
