e67d718060db62341ba79f4bc4b26a1e13663fb8
Andrew Mustea <amustea224@gmail.com>
2023-03-08 18:51:26 -0800

    setup: github-cli: add script to install github-cli
    Arch Linux can use `pacman` to install from the community repo.
    
    Debian/Ubuntu will need to add github to its apt source list to install
    github-cli.

---
+++ setup/github-cli.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#
+# ./setup/github-cli.sh
+#
+
+# install the github-cli tool
+
+# check if github-cli is installed
+if hash gh &>/dev/null; then
+  echo "github-cli is already installed"
+  exit 0
+fi
+
+# error message
+error-msg() {
+  echo -e "$(tput setaf 1)Error$(tput sgr0): $*" 1>&2
+}
+
+# check if user is root
+if (( EUID == 0 )); then
+  error-msg "don't run script as root"
+  exit 1
+fi
+
+# get os name
+NAME="$(grep "^ID=" /etc/os-release | awk -F '=' '{ print $2 }')"
+readonly NAME
+
+# --------------------
+# arch linux
+# --------------------
+
+if [[ "${NAME}" == "arch" ]]; then
+  sudo pacman -S --noconfirm --needed github-cli
+  exit "$?"
+fi
+
+# --------------------
+# debian/ubuntu
+# --------------------
+
+if [[ "${NAME}" == "debian" ]] || [[ "${NAME}" == "ubuntu" ]]; then
+  SOURCE="https://cli.github.com/packages/githubcli-archive-keyring.gpg"
+  KEYRING="/usr/share/keyrings/githubcli-archive-keyring.gpg"
+  ARCH="$(dpkg --print-architecture)"
+  LIST="/etc/apt/sources.list.d/github-cli.list"
+  REPO="https://cli.github.com/packages"
+  readonly SOURCE KEYRING ARCH LIST REPO
+
+  # download key
+  if ! sudo curl -fsSL --create-dirs "${SOURCE}" -o "${KEYRING}"; then
+    error-msg "couldn't download github-cli apt keyring"
+    exit 1
+  fi
+
+  # add read permissions to keyring for group and owner
+  sudo chmod go+r "${KEYRING}"
+
+  # add github-cli to apt source list
+  echo "deb [arch=${ARCH} signed-by=${KEYRING}] ${REPO} stable main" | sudo tee "${LIST}"
+
+  # update apt package info and install github-cli
+  if hash nala &>/dev/null; then
+    sudo nala update && sudo nala install -y gh
+  else
+    sudo apt update && sudo apt install -y gh
+  fi
+
+  exit "$?"
+fi
+
+# --------------------
+# other
+# --------------------
+
+error-msg "unsupported distribution: '${NAME}'"
+exit 1
+
