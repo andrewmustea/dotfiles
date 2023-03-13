7495e935443fe2a89230d535471ccfd612b10b17
Andrew Mustea <amustea224@gmail.com>
2023-03-13 16:27:30 -0700

    setup: nvm: update and refactor install script

--- setup/nvm.sh
+++ setup/nvm.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
 if declare -f -F nvm > /dev/null; then
-    echo "nvm already installed"
-    exit 0
+  echo "nvm already installed"
+  exit 0
 fi
 
-DISTRO="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
+# get os name
+NAME="$(grep "^ID=" /etc/os-release | awk -F '=' '{ print $2 }')"
+readonly NAME
 
+# XDG directory
 [[ -z "${XDG_CONFIG_HOME}" ]] && export XDG_CONFIG_HOME="${HOME}/.config"
 
 export NVM_DIR="${XDG_CONFIG_HOME}/nvm"
 
-if [[ "$DISTRO" = "arch" ]]; then
-    sudo pacman -S --needed --noconfirm nvm
-    mkdir --parents "${NVM_DIR}"
-    source /usr/share/nvm/init-nvm.sh
-    exit 0
+# arch linux
+if [[ "${NAME}" = "arch" ]]; then
+  sudo pacman -S --needed --noconfirm nvm
+  mkdir --parents "${NVM_DIR}"
+  source /usr/share/nvm/init-nvm.sh
+  exit 0
 fi
 
+# other distros
 git clone https://github.com/nvm-sh/nvm.git "${NVM_DIR}"
 
+# source files
 [[ -s "${NVM_DIR}/nvm.sh" ]] && source "${NVM_DIR}/nvm.sh"
 [[ -s "${NVM_DIR}/bash_completion" ]] && source "${NVM_DIR}/bash_completion"
 
+# install node and npm
 nvm install node --default --latest-npm
 
-npm install -g npm@latest neovim commitizen markdownlint
+# install packages
+npm install -g neovim markdownlint
 
