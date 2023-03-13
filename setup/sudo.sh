51b2fc49a8d8fe710e3db8c159cc684dcee1cb40
Andrew Mustea <amustea224@gmail.com>
2023-03-13 16:28:05 -0700

    setup: sudo: update and refactor install script

--- setup/sudo.sh
+++ setup/sudo.sh
@@ -7,11 +7,8 @@
 # install sudo v1.19 manually for ubuntu systems
 
 # error message
-readonly RED='\033[0;31m'
-readonly WHT='\033[0;37m'
-
 error-msg() {
-  printf "%b: %b\n" "${RED}Error${WHT}" "$*" 1>&2
+  echo -e "$(tput setaf 1)Error$(tput sgr0): $*" 1>&2
 }
 
 # get os name
@@ -28,7 +25,7 @@ fi
 VERSION="$(sudo --version | awk -F'[ .]' 'NR==1{ print $4 }')"
 readonly VERSION
 if (( VERSION >= 9 )); then
-  echo "sudo v1.9 already installed"
+  echo "sudo v1.9+ already installed"
   exit 0
 fi
 
@@ -38,32 +35,31 @@ if (( EUID != 0 )); then
   exit 1
 fi
 
-# package manager
-if hash nala &>/dev/null; then
-  PACKAGE_MANAGER=nala
-else
-  PACKAGE_MANAGER=apt
-fi
-readonly PACKAGE_MANAGER
-
-# set variables
-ARCH="$(dpkg --print-architecture)"
-DIR=/tmp/sudo
+# github-cli variables
+TEMP="/tmp/sudo"
 PROJECT="sudo-project/sudo"
 RELEASE="$(lsb_release -r | awk -F ':\t' '{ gsub("\\.", "", $2); print $2 }')"
-PATTERN="sudo_[0-9].*ubu${RELEASE}_${ARCH}\.deb"
-readonly ARCH DIR PROJECT RELEASE PATTERN
+PATTERN="sudo_[0-9].*ubu${RELEASE}_$(dpkg --print-architecture)\.deb"
+readonly TEMP PROJECT RELEASE PATTERN
+
+# create temporary directory
+rm -rf "${TEMP}"
+mkdir "${TEMP}"
 
 # download package
-rm -rf "${DIR}" && mkdir -p "${DIR}"
-if ! gh release download --skip-existing -R "${PROJECT}" -p "${PATTERN}" -D "${DIR}"; then
-  rm -rf "${DIR}"
+if ! gh release download --skip-existing -R "${PROJECT}" -p "${PATTERN}" -D "${TEMP}"; then
+  rm -rf "${TEMP}"
   exit 1
 fi
 
 # install package
-"${PACKAGE_MANAGER}" install "${DIR}"/*
+PACKAGE="$(ls -d "${TEMP}")"
+readonly PACKAGE
+if ! dpkg -i "${PACKAGE}"; then
+  error-msg "couldn't install package: ${PACKAGE}"
+  exit 1
+fi
 
 # clean up /tmp/sudo
-rm -rf "${DIR}"
+rm -rf "${TEMP}"
 
