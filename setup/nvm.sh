dc86df1f1e8180519e2cc8fb0bf0d70f62f28d1f
Andrew Mustea <amustea224@gmail.com>
2023-03-13 16:27:30 -0700

    setup: nvm: update and refactor install script

--- setup/nvm.sh
+++ setup/nvm.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
+# check if nvm is installed
 if declare -f -F nvm > /dev/null; then
-    echo "nvm already installed"
-    exit 0
+  echo "nvm already installed"
+  exit 0
 fi
 
-DISTRO="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
+# error message
+error-msg() {
+  echo -e "$(tput setaf 1)Error$(tput sgr0): $*" 1>&2
+}
 
+# check if user is root
+if (( EUID == 0 )); then
+  error-msg "don't run script as root"
+  exit 1
+fi
+
+# get os name
+NAME="$(grep "^ID=" /etc/os-release | awk -F '=' '{ print $2 }')"
+readonly NAME
+
+# XDG directory
 [[ -z "${XDG_CONFIG_HOME}" ]] && export XDG_CONFIG_HOME="${HOME}/.config"
 
+# nvm directory
 export NVM_DIR="${XDG_CONFIG_HOME}/nvm"
 
-if [[ "$DISTRO" = "arch" ]]; then
-    sudo pacman -S --needed --noconfirm nvm
-    mkdir --parents "${NVM_DIR}"
-    source /usr/share/nvm/init-nvm.sh
-    exit 0
+# arch linux
+if [[ "${NAME}" = "arch" ]]; then
+  sudo pacman -S --needed --noconfirm nvm
+  mkdir --parents "${NVM_DIR}"
+  source /usr/share/nvm/init-nvm.sh
+  exit 0
+else
+  git clone https://github.com/nvm-sh/nvm.git "${NVM_DIR}"
+
+  # source files
+  [[ -s "${NVM_DIR}/nvm.sh" ]] && source "${NVM_DIR}/nvm.sh"
+  [[ -s "${NVM_DIR}/bash_completion" ]] && source "${NVM_DIR}/bash_completion"
 fi
 
-git clone https://github.com/nvm-sh/nvm.git "${NVM_DIR}"
-
-[[ -s "${NVM_DIR}/nvm.sh" ]] && source "${NVM_DIR}/nvm.sh"
-[[ -s "${NVM_DIR}/bash_completion" ]] && source "${NVM_DIR}/bash_completion"
-
+# install node and npm
 nvm install node --default --latest-npm
 
-npm install -g npm@latest neovim commitizen markdownlint
+# install packages
+npm install -g neovim markdownlint
 
