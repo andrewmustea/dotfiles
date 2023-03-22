7de68a90be5aad29ad894c40fedc830d0bd52e01
Andrew Mustea <amustea224@gmail.com>
2023-03-22 12:59:49 -0700

    setup: nala: add support for other debian based distros

--- setup/nala.sh
+++ setup/nala.sh
@@ -13,12 +13,20 @@ if hash nala &>/dev/null; then
 fi
 
 # error message
-readonly RED='\033[0;31m'
-readonly WHT='\033[0;37m'
-
 error-msg() {
-  printf "%b: %b\n" "${RED}Error${WHT}" "$*" 1>&2
+  echo -e "$(tput setaf 1)Error$(tput sgr0): $*" 1>&2
 }
+
+# get distro name
+NAME="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
+ID_LIKE="$(grep "^ID_LIKE=" /etc/os-release | awk -F "=" '{ print $2 }')"
+readonly NAME ID_LIKE
+
+# check if system is debian or debian-based
+if [[ "${NAME}" != "debian" ]] && [[ "${ID_LIKE}" != "debian" ]]; then
+  error-msg "nala requires a debian or debian-based distro"
+  exit 1
+fi
 
 # check if user is root
 if (( EUID == 0 )); then
@@ -26,40 +34,35 @@ if (( EUID == 0 )); then
   exit 1
 fi
 
-# get distro name
-NAME="$(grep "^ID=" /etc/os-release | awk -F "=" '{ print $2 }')"
-readonly NAME
-
-# check distro version
-unset RELEASE LEGACY
+# determine nala version to install
+NALA_VER="nala"
 if [[ "${NAME}" == "ubuntu" ]]; then
   RELEASE="$(grep "^VERSION_ID=" /etc/os-release | awk -F '[".]' '{ print $2 }')"
+  readonly RELEASE
   if (( RELEASE < 20 )); then
-    error-msg "nala requires Ubuntu release >=20.04"
+    error-msg "nala requires ubuntu 20.04+"
     exit 1
   elif (( RELEASE < 22 )); then
-    LEGACY=1
+    NALA_VER="nala-legacy"
   fi
-elif [[ "${NAME}" != "debian" ]]; then
-  error-msg "nala requires a debian or debian-based distro"
+fi
+readonly NALA_VER
+
+# download apt keyring
+readonly SOURCE="https://deb.volian.org/volian/scar.key"
+readonly KEYRING="/etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg"
+if ! wget -qO - "${SOURCE}" | sudo tee "${KEYRING}" > /dev/null; then
+  error-msg "couldn't download nala apt keyring"
   exit 1
 fi
-readonly RELEASE LEGACY
 
-# add source files
-echo "deb https://deb.volian.org/volian/ scar main" |
-  sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-wget -qO - https://deb.volian.org/volian/scar.key |
-  sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+# add apt source list
+readonly REPO="https://deb.volian.org/volian/"
+readonly LIST="/etc/apt/sources.list.d/volian-archive-scar-unstable.list"
+echo "deb ${REPO} scar main" | sudo tee "${LIST}" > /dev/null
 
 # install nala
-sudo apt update
-if [[ -n "${LEGACY}" ]]; then
-  echo "Installing nala-legacy..."
-  sudo apt install -y nala-legacy
-else
-  echo "Installing nala..."
-  sudo apt install -y nala
-fi
+sudo apt update && sudo apt install -y "${NALA_VER}"
+
 exit "$?"
 
