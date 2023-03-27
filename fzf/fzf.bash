3ac964a8d4b317f38c52cfde9e9ac997edeb7782
Andrew Mustea <amustea224@gmail.com>
2023-03-27 11:26:57 -0700

    fzf: add update function for local git fzf installs
    Also clean up variables after using them.

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -13,21 +13,30 @@ fi
 # XDG directory
 [[ -z "${XDG_DATA_HOME}" ]] && export XDG_DATA_HOME="${HOME}/.local/share"
 
-# set paths
-unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH
+# possible fzf paths
 LOCAL_FZF="${XDG_DATA_HOME}/fzf/bin"
 SYSTEM_FZF="/usr/share/fzf"
+
+# get fzf paths depending on installation method
+unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH
 if [[ -d "${LOCAL_FZF}" ]]; then
   FZF_PATH="${LOCAL_FZF}"
   AUTOCOMPLETE="${XDG_DATA_HOME}/fzf/shell/completion.bash"
   KEY_BINDINGS="${XDG_DATA_HOME}/fzf/shell/key-bindings.bash"
+
+  # update local git fzf
+  update-fzf() {
+    git -C "${XDG_DATA_HOME}/fzf" checkout master
+    git -C "${XDG_DATA_HOME}/fzf" pull
+    "${FZF_DIR}/install" --xdg --bin
+  }
 elif [[ -d "${SYSTEM_FZF}" ]]; then
   FZF_PATH="${SYSTEM_FZF}"
   AUTOCOMPLETE="${SYSTEM_FZF}/completion.bash"
   KEY_BINDINGS="${SYSTEM_FZF}/key-bindings.bash"
 else
   echo "no fzf directory found" 1>&2
-  unset AUTOCOMPLETE KEY_BINDINGS
+  unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH LOCAL_FZF SYSTEM_FZF
   return 1
 fi
 
@@ -53,5 +62,5 @@ if [[ "${FZF_DEFAULT_OPTS}" != *color* ]]; then
   export FZF_DEFAULT_OPTS+=" ${OPTS[*]}"
 fi
 
-unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH
+unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH LOCAL_FZF SYSTEM_FZF
 
