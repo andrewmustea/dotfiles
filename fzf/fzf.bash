d08b597abf06b0797ec16c9d558870a912512761
Andrew Mustea <amustea224@gmail.com>
2023-07-18 11:37:27 -0700

    fzf: bash: add homebrew install option
    Remove fzf addition to path.

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -6,7 +6,7 @@
 
 # check if fzf is available
 if ! hash fzf &>/dev/null; then
-  echo "no fzf binary found" 1>&2
+  echo "fzf not found" 1>&2
   return 1
 fi
 
@@ -14,44 +14,40 @@ fi
 [[ -z "${XDG_DATA_HOME}" ]] && export XDG_DATA_HOME="${HOME}/.local/share"
 
 # possible fzf paths
-LOCAL_FZF="${XDG_DATA_HOME}/fzf/bin"
-SYSTEM_FZF="/usr/share/fzf"
+local_fzf="${XDG_DATA_HOME}/fzf"
+homebrew_fzf="$(brew --prefix)/opt/fzf"
+system_fzf="/usr/share/fzf"
 
 # get fzf paths depending on installation method
-unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH
-if [[ -d "${LOCAL_FZF}" ]]; then
-  FZF_PATH="${LOCAL_FZF}"
-  AUTOCOMPLETE="${XDG_DATA_HOME}/fzf/shell/completion.bash"
-  KEY_BINDINGS="${XDG_DATA_HOME}/fzf/shell/key-bindings.bash"
+unset autocomplete key_bindings fzf_path
+if [[ -d "${local_fzf}" ]]; then
+  autocomplete="${local_fzf}/shell/completion.bash"
+  key_bindings="${local_fzf}/shell/key-bindings.bash"
 
   # update local git fzf
   update-fzf() {
-    git -C "${XDG_DATA_HOME}/fzf" checkout master
     git -C "${XDG_DATA_HOME}/fzf" pull
-    "${FZF_DIR}/install" --xdg --bin
+    "${XDG_DATA_HOME}/fzf/install" --xdg --bin
   }
-elif [[ -d "${SYSTEM_FZF}" ]]; then
-  FZF_PATH="${SYSTEM_FZF}"
-  AUTOCOMPLETE="${SYSTEM_FZF}/completion.bash"
-  KEY_BINDINGS="${SYSTEM_FZF}/key-bindings.bash"
+elif [[ -d "${homebrew_fzf}" ]]; then
+  autocomplete="${homebrew_fzf}/script/completion.bash"
+  key_bindings="${homebrew_fzf}/script/key-bindings.bash"
+elif [[ -d "${system_fzf}" ]]; then
+  autocomplete="${system_fzf}/completion.bash"
+  key_bindings="${system_fzf}/key-bindings.bash"
 else
   echo "no fzf directory found" 1>&2
-  unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH LOCAL_FZF SYSTEM_FZF
+  unset autocomplete key_bindings fzf_path local_fzf system_fzf
   return 1
-fi
-
-# add fzf to path
-if [[ ":${PATH}:" != ":${FZF_PATH}:" ]]; then
-  export PATH="${FZF_PATH}${PATH:+":${PATH}"}"
 fi
 
 # auto-completion
 if [[ "$-" == *i* ]]; then
-  source "${AUTOCOMPLETE}" 2>/dev/null
+  source "${autocomplete}" 2>/dev/null
 fi
 
 # key bindings
-source "${KEY_BINDINGS}"
+source "${key_bindings}"
 
 # colors
 if [[ "${FZF_DEFAULT_OPTS}" != *color* ]]; then
@@ -59,8 +55,9 @@ if [[ "${FZF_DEFAULT_OPTS}" != *color* ]]; then
         "--color=fg+:#bbbbbb,bg+:#151a1e,hl+:#36a3d9"
         "--color=info:#508040,prompt:#8030e0,pointer:#f06722"
         "--color=marker:#0040bb,spinner:#b02828,header:#0078c8")
-  export FZF_DEFAULT_OPTS+=" ${OPTS[*]}"
+  FZF_DEFAULT_OPTS+=" ${OPTS[*]}"
+  export FZF_DEFAULT_OPTS
 fi
 
-unset AUTOCOMPLETE KEY_BINDINGS FZF_PATH LOCAL_FZF SYSTEM_FZF
+unset autocomplete key_bindings fzf_path local_fzf system_fzf
 
