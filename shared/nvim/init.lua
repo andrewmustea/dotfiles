0674142df396585bf679a5cf1279406cd27e8e21
Andrew Mustea <amustea224@gmail.com>
2024-02-08 16:36:19 -0800

    feat(nvim): move to lazy.nvim plugin manager

--- shared/nvim/init.lua
+++ shared/nvim/init.lua
@@ -10,11 +10,23 @@ require("settings")
 -- autocommands and functions
 require("autocmds")
 
--- plugins
-require("plugins")
-
 -- keybinds
 require("keybinds")
 
 -- filetypes
 require("filetypes")
+
+-- plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
