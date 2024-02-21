fc16d6f11eb52643333595fc14bc3441fae65217
Andrew Mustea <amustea224@gmail.com>
2024-02-20 18:11:13 -0800

    fix(fzf): have system_fzf.bash only handle system wide fzf installs

--- shared/fzf/system_fzf.bash
+++ shared/fzf/system_fzf.bash
@@ -10,60 +10,32 @@ if ! hash fzf &>/dev/null; then
   return 1
 fi
 
-# XDG directory
-[[ -z "${XDG_DATA_HOME}" ]] && export XDG_DATA_HOME="${HOME}/.local/share"
-
-# possible fzf paths
-local_fzf="${XDG_DATA_HOME}/fzf"
-homebrew_fzf="$(brew --prefix)/opt/fzf"
-system_fzf="/usr/share/fzf"
-
-# get fzf paths depending on installation method
-unset autocomplete key_bindings fzf_path
-if [[ -d "${local_fzf}" ]]; then
-  autocomplete="${local_fzf}/shell/completion.bash"
-  key_bindings="${local_fzf}/shell/key-bindings.bash"
-
-  # update local git fzf
-  update-fzf() {
-    git -C "${XDG_DATA_HOME}/fzf" pull
-    "${XDG_DATA_HOME}/fzf/install" --xdg --bin
-  }
-elif [[ -d "${homebrew_fzf}" ]]; then
-  autocomplete="${homebrew_fzf}/script/completion.bash"
-  key_bindings="${homebrew_fzf}/script/key-bindings.bash"
-elif [[ -d "${system_fzf}" ]]; then
-  autocomplete="${system_fzf}/completion.bash"
-  key_bindings="${system_fzf}/key-bindings.bash"
-else
-  echo "no fzf directory found" 1>&2
-  unset autocomplete key_bindings fzf_path local_fzf system_fzf
+# check if system fzf install exists
+if [[ ! -d /usr/share/fzf ]]; then
+  echo "couldn't find system fzf directory at '/usr/share/fzf'" 1>&2
   return 1
 fi
 
 # auto-completion
 if [[ "$-" == *i* ]]; then
-  source "${autocomplete}" 2>/dev/null
+  source "/usr/share/fzf/completion.bash" 2>/dev/null
 fi
 
 # key bindings
-source "${key_bindings}"
+source "/usr/share/fzf/key-bindings.bash"
 
-# colors
 if [[ "${FZF_DEFAULT_OPTS}" != *color* ]]; then
-  colors=( "--color=fg:#888888,hl:#b030a0"
-           "--color=fg+:#bbbbbb,bg+:#151a1e,hl+:#36a3d9"
-           "--color=info:#508040,prompt:#8030e0,pointer:#f06722"
-           "--color=marker:#0040bb,spinner:#b02828,header:#0078c8" )
-  FZF_DEFAULT_OPTS+=" ${colors[*]}"
+  FZF_DEFAULT_OPTS+=(
+    "--color=fg:#888888,hl:#b030a0"
+    "--color=fg+:#bbbbbb,bg+:#151a1e,hl+:#36a3d9"
+    "--color=info:#508040,prompt:#8030e0,pointer:#f06722"
+    "--color=marker:#0040bb,spinner:#b02828,header:#0078c8"
+  )
   export FZF_DEFAULT_OPTS
-  unset colors
 fi
-
-unset autocomplete key_bindings fzf_path local_fzf system_fzf
 
 # default commands
 if hash fd &>/dev/null; then
-  export FZF_DEFAULT_COMMAND='command fd -HIi --type file'
+  export FZF_DEFAULT_COMMAND="command fd -HIi --type file"
   export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 fi
